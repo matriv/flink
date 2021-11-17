6a45b82ba2249e4a18d7bab79e715ecf4e0e0f94
Marios Trivyzas <matriv@gmail.com>
2021-11-17 14:33:24 +0100

    rename antlr4 -> antlr

--- flink-table/flink-table-code-splitter/build.gradle.kts
+++ flink-table/flink-table-code-splitter/build.gradle.kts
@@ -13,16 +13,17 @@ dependencies {
     testImplementation(project(":flink-test-utils-junit"))
     api(project(":flink-streaming-java"))
     add("antlr", "org.antlr:antlr4:4.7.2")
+    antlr("org.antlr:antlr4:4.7.1")
 }
 
 description = "Flink : Table : Code Splitter"
 
-sourceSets["main"].antlr.srcDirs(file("${projectDir}/src/main/antlr4"))
+//sourceSets["main"].antlr.srcDirs(file("${projectDir}/src/main/antlr4"))
 
 
 tasks.named<AntlrTask>("generateGrammarSource") {
     maxHeapSize = "64m"
     arguments = listOf("-visitor")
-    outputDirectory = file("${buildDir}/generated-sources/antlr4/org/apache/flink/table/codesplit")
+    //outputDirectory = file("${buildDir}/generated-sources/antlr4/org/apache/flink/table/codesplit")
 }
 
