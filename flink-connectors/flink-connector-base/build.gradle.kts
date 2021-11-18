a244c5f6dcd19c90f4312fbf66bd10befd212e91
Marios Trivyzas <matriv@gmail.com>
2021-11-18 18:10:42 +0100

    flink-connector-base

--- flink-connectors/flink-connector-base/build.gradle.kts
+++ flink-connectors/flink-connector-base/build.gradle.kts
@@ -7,10 +7,16 @@ plugins {
 }
 
 dependencies {
-    testImplementation(project(":flink-core"))
+    implementation(project(":flink-core"))
     testImplementation(project(":flink-test-utils"))
+    testImplementation(project(":flink-test-utils-junit"))
     testImplementation(project(":flink-connector-test-utils"))
-    compileOnly(project(":flink-core"))
+    testImplementation(project(":flink-streaming-java"))
+    compileOnly(project(":flink-annotations"))
+    implementation(project(":flink-runtime"))
+    implementation(project(":flink-runtime", "testArtifacts"))
+    implementation(project(":flink-core", "testArtifacts"))
+    testImplementation("org.apache.commons:commons-lang3:3.3.2")
 }
 
 description = "Flink : Connectors : Base"
