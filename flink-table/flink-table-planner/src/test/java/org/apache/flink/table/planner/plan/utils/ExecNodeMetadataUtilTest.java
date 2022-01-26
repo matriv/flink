e7412dca5d18368d2c32968af3ae4d75a5585c63
Marios Trivyzas <matriv@gmail.com>
2022-01-26 14:09:03 +0200

    [hotfix][table-planner] Remove org.reflections usage and dependency

--- flink-table/flink-table-planner/src/test/java/org/apache/flink/table/planner/plan/utils/ExecNodeMetadataUtilTest.java
+++ flink-table/flink-table-planner/src/test/java/org/apache/flink/table/planner/plan/utils/ExecNodeMetadataUtilTest.java
@@ -22,11 +22,20 @@ import org.apache.flink.FlinkVersion;
 import org.apache.flink.api.dag.Transformation;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.planner.delegation.PlannerBase;
+import org.apache.flink.table.planner.plan.nodes.exec.ExecNode;
 import org.apache.flink.table.planner.plan.nodes.exec.ExecNodeBase;
 import org.apache.flink.table.planner.plan.nodes.exec.ExecNodeContext;
 import org.apache.flink.table.planner.plan.nodes.exec.ExecNodeMetadata;
 import org.apache.flink.table.planner.plan.nodes.exec.ExecNodeMetadatas;
 import org.apache.flink.table.planner.plan.nodes.exec.InputProperty;
+import org.apache.flink.table.planner.plan.nodes.exec.serde.JsonSerdeUtil;
+import org.apache.flink.table.planner.plan.nodes.exec.stream.StreamExecDataStreamScan;
+import org.apache.flink.table.planner.plan.nodes.exec.stream.StreamExecGroupTableAggregate;
+import org.apache.flink.table.planner.plan.nodes.exec.stream.StreamExecLegacySink;
+import org.apache.flink.table.planner.plan.nodes.exec.stream.StreamExecLegacyTableSourceScan;
+import org.apache.flink.table.planner.plan.nodes.exec.stream.StreamExecMultipleInput;
+import org.apache.flink.table.planner.plan.nodes.exec.stream.StreamExecPythonGroupTableAggregate;
+import org.apache.flink.table.planner.plan.nodes.exec.stream.StreamExecSort;
 import org.apache.flink.table.types.logical.LogicalType;
 
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonCreator;
@@ -34,23 +43,39 @@ import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonCre
 import org.assertj.core.api.Condition;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.Assert.assertTrue;
 
 /** Test for {@link ExecNodeMetadataUtil}. */
 public class ExecNodeMetadataUtilTest {
+
+    @SuppressWarnings("rawtypes")
+    private static final List<Class<? extends ExecNode>> UNSUPPORTED_JSON_SERDE_CLASSES =
+            Arrays.asList(
+                    StreamExecDataStreamScan.class,
+                    StreamExecLegacyTableSourceScan.class,
+                    StreamExecLegacySink.class,
+                    StreamExecGroupTableAggregate.class,
+                    StreamExecPythonGroupTableAggregate.class,
+                    StreamExecSort.class,
+                    StreamExecMultipleInput.class);
 
     @Test
     public void testNoJsonCreator() {
         assertThatThrownBy(() -> ExecNodeMetadataUtil.addTestNode(DummyNodeNoJsonCreator.class))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage(
-                        "ExecNode: org.apache.flink.table.planner.plan.utils." +
-                                "ExecNodeMetadataUtilTest.DummyNodeNoJsonCreator does not " +
-                                "implement @JsonCreator annotation on constructor. This is a " +
-                                "bug, please contact developers.");
+                        "ExecNode: org.apache.flink.table.planner.plan.utils."
+                                + "ExecNodeMetadataUtilTest.DummyNodeNoJsonCreator does not "
+                                + "implement @JsonCreator annotation on constructor. This is a "
+                                + "bug, please contact developers.");
     }
 
     @Test
@@ -58,10 +83,10 @@ public class ExecNodeMetadataUtilTest {
         assertThatThrownBy(() -> ExecNodeMetadataUtil.addTestNode(DummyNodeNoAnnotation.class))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage(
-                        "ExecNode: org.apache.flink.table.planner.plan.utils." +
-                                "ExecNodeMetadataUtilTest.DummyNodeNoAnnotation is missing " +
-                                "ExecNodeMetadata annotation. This is a bug, please contact " +
-                                "developers.");
+                        "ExecNode: org.apache.flink.table.planner.plan.utils."
+                                + "ExecNodeMetadataUtilTest.DummyNodeNoAnnotation is missing "
+                                + "ExecNodeMetadata annotation. This is a bug, please contact "
+                                + "developers.");
     }
 
     @Test
@@ -69,12 +94,12 @@ public class ExecNodeMetadataUtilTest {
         assertThatThrownBy(() -> ExecNodeMetadataUtil.addTestNode(DummyNodeBothAnnotations.class))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage(
-                        "ExecNode: org.apache.flink.table.planner.plan.utils." +
-                                "ExecNodeMetadataUtilTest.DummyNodeBothAnnotations is annotated "+
-                                "both with interface org.apache.flink.table.planner.plan.nodes." +
-                                "exec.ExecNodeMetadata and interface org.apache.flink.table." +
-                                "planner.plan.nodes.exec.ExecNodeMetadatas. This is a bug, " +
-                                "please contact developers.");
+                        "ExecNode: org.apache.flink.table.planner.plan.utils."
+                                + "ExecNodeMetadataUtilTest.DummyNodeBothAnnotations is annotated "
+                                + "both with interface org.apache.flink.table.planner.plan.nodes."
+                                + "exec.ExecNodeMetadata and interface org.apache.flink.table."
+                                + "planner.plan.nodes.exec.ExecNodeMetadatas. This is a bug, "
+                                + "please contact developers.");
     }
 
     @Test
@@ -92,6 +117,40 @@ public class ExecNodeMetadataUtilTest {
                 .has(
                         new Condition<>(
                                 m -> m.minPlanVersion() == FlinkVersion.v1_15, "minStateVersion"));
+    }
+
+    @Test
+    public void testStreamExecNodeJsonSerdeCoverage() {
+        Set<Class<? extends ExecNode<?>>> subClasses = ExecNodeMetadataUtil.execNodes();
+        List<Class<? extends ExecNode<?>>> classesWithoutJsonCreator = new ArrayList<>();
+        List<Class<? extends ExecNode<?>>> classesWithJsonCreatorInUnsupportedList =
+                new ArrayList<>();
+        for (Class<? extends ExecNode<?>> clazz : subClasses) {
+            boolean hasJsonCreator = JsonSerdeUtil.hasJsonCreatorAnnotation(clazz);
+            if (hasJsonCreator && UNSUPPORTED_JSON_SERDE_CLASSES.contains(clazz)) {
+                classesWithJsonCreatorInUnsupportedList.add(clazz);
+            }
+            if (!hasJsonCreator && !UNSUPPORTED_JSON_SERDE_CLASSES.contains(clazz)) {
+                classesWithoutJsonCreator.add(clazz);
+            }
+        }
+        assertTrue(
+                String.format(
+                        "%s do not support json serialization/deserialization, "
+                                + "please refer the implementation of the other StreamExecNodes.",
+                        classesWithoutJsonCreator.stream()
+                                .map(Class::getSimpleName)
+                                .collect(Collectors.joining(","))),
+                classesWithoutJsonCreator.isEmpty());
+        assertTrue(
+                String.format(
+                        "%s have support json serialization/deserialization, "
+                                + "but still in UNSUPPORTED_JSON_SERDE_CLASSES list. "
+                                + "please move them from UNSUPPORTED_JSON_SERDE_CLASSES.",
+                        classesWithJsonCreatorInUnsupportedList.stream()
+                                .map(Class::getSimpleName)
+                                .collect(Collectors.joining(","))),
+                classesWithJsonCreatorInUnsupportedList.isEmpty());
     }
 
     @ExecNodeMetadatas({
